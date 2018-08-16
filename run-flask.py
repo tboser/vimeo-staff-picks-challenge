d4bc20df7560042f086bd910c464965e37fc716e
tboser <thomasboser@gmail.com>
2018-08-16 14:58:58 -0700

    Improvements to web app

--- run-flask.py
+++ run-flask.py
@@ -2,14 +2,16 @@
 
 """ Put docstring here. """  # TODO
 
-from flask import Flask, render_template, request, flash  # TODO - remove unused imports
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from flask import Flask, render_template, request, flash
+from wtforms import Form, TextField
 from elasticsearch_dsl.connections import connections
 
-from vimeo_challenge.index import get_similar_clips, load_model
+from vimeo_challenge.index import get_similar_clips, load_model, get_clip_by_id
 
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '12345'
 
 
 class ReusableForm(Form):
@@ -17,23 +19,30 @@ class ReusableForm(Form):
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/prompt', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def prompt():
+    """ Display page prompting for clip ID. """
     form = ReusableForm(request.form)
+    connections.create_connection(hosts=['elasticsearch:9200'])
 
     if request.method == 'POST':
         clip_id = form.clip_id.data
-        return display(clip_id)
+        clip = get_clip_by_id(clip_id)
+        if clip is not None:
+            similar_clips = get_similar_clips(clip_id)
+            return render_template('index.html', form=form, clips=similar_clips)
+        else:
+            flash('Clip ID not found. Please enter a new clip ID.')
 
-    return render_template('prompt.html', form=form)
+    return render_template('index.html', form=form, clips=[])
 
 
-@app.route('/display')
-def display(clip_id):
+def setup():
+    """ Restore ES snapshot before using web app. """
     load_model(host='elasticsearch')
-    connections.create_connection(hosts=['elasticsearch:9200'])
-    similar_clips = get_similar_clips(clip_id)
-    return render_template('display.html', clips=similar_clips)
+
+
+app.before_first_request(setup)
 
 
 if __name__ == '__main__':
